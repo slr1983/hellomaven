a8c8734381b87d2a14be3d20954a032ab6d4f9da
Sumit R <sumit.l.ramteke@gmail.com>
2021-12-30 17:28:36 +0530

    Add New Files for Jenkins GIT Demo

---
+++ src/main/java/com/sumit/maven/demo/App.java
@@ -0,0 +1,13 @@
+package com.sumit.maven.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
